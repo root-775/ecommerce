55ca13a52d0bf2e557991b991fccd0b299b93362
root-775 <webdeveloper.amitgupta@gmail.com>
2023-04-04 11:44:03 +0530

    first commit

--- resources/js/stores/product.store.js
+++ resources/js/stores/product.store.js
@@ -2,11 +2,7 @@ import { defineStore } from "pinia";
 import { fetchWrapper } from '../halpers/fetch-wrapper.js';
 import { ref } from "vue";
 
-<<<<<<< HEAD
-const baseUrl = `${window.website.apiUrl}/product`;
-=======
 const baseUrl = `${window.website.url}/api/product`;
->>>>>>> 782ec0653ac1ba13dc3eba0dd7622bce3c53c906
 
 export const useProductStore = defineStore("product", () => {
     const products = ref({});
@@ -19,9 +15,6 @@ export const useProductStore = defineStore("product", () => {
         }
     }
 
-<<<<<<< HEAD
-    return { products, getAllProducts}
-=======
     async function getSingleProduct(id) {
         try {
             const result = await fetchWrapper.get(`${baseUrl}/${id}`);
@@ -32,5 +25,4 @@ export const useProductStore = defineStore("product", () => {
     }
 
     return { products, getAllProducts, getSingleProduct}
->>>>>>> 782ec0653ac1ba13dc3eba0dd7622bce3c53c906
 });
